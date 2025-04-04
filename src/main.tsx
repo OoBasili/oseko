ad8d054c0c1897ac2416e05222d843543aacc639
OoBasili <vasilok11@gmail.com>
2025-04-04 11:32:34 +0300

    Render sphere

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,5 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from 'react-dom/client';
+import { MainCanvas } from './canvas';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = document.getElementById('root');
+root && createRoot(root).render(<MainCanvas />);
